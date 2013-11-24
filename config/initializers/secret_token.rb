840b8787146c4b72af99c6d36954b8fdc0dc44b5
System Administrator <root@Sutees-MacBook-Pro.local>
2013-11-23 17:04:35 -0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NestedFormWithMultiTableInheritance::Application.config.secret_key_base = '3e48ada4d642cfc5367df1a3b5f4a56fb1924394a7857e439c39a20f07bdc310a36f33d03c0a08a74c584b4053ec40d288ddf48e44131f49ab510a9ca766cc38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NestedFormWithMultiTableInheritance::Application.config.session_store :cookie_store, key: '_nested-form-with-multi-table-inheritance_session'
