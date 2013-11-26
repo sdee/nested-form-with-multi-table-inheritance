2c4a2cb74f31dff3e64e9d34d5b49f8c514fc574
Sutee Dee <thaiyoshi@Sutees-MacBook-Pro.local>
2013-11-26 01:55:53 -0800

    fixed conflict

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,10 +2,7 @@ class Product < ActiveRecord::Base
 
 	 acts_as_superclass
 	 belongs_to :store
-<<<<<<< HEAD
 	 has_one :book
 	 accepts_nested_attributes_for :book, :allow_destroy => true, :reject_if => :all_blank
 
-=======
->>>>>>> 548aadee0aaaa675e0c679678293fb472734eedd
 end
